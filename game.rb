1efac2db2b74584b42d2ac1518ae060a97de2f5a
JP <85654561+jp524@users.noreply.github.com>
2022-08-02 14:04:36 -0400

    Show current player positions on board

--- game.rb
+++ game.rb
@@ -2,11 +2,11 @@ class Board
   attr_reader :positions
 
   def initialize
-    @positions = [['a1', 'b1', 'c1'], ['a2', 'b2', 'c2'], ['a3', 'b3', 'c3']]
+    @positions = ['a1', 'b1', 'c1', 'a2', 'b2', 'c2', 'a3', 'b3', 'c3']
   end
 
   def display_grid
-    @positions.map do |row|
+    @positions.each_slice(3).map do |row|
       row.join(' | ')
     end.join("\n------------\n")
   end
@@ -20,8 +20,15 @@ class Player
     @choices = choices
     @board = board
   end
+
+  def positions_on_board
+    @board.positions.map! do |position|
+      @choices.include?(position) ? @marker : position
+    end
+  end
 end
 
 board = Board.new
 player1 = Player.new('X', ['a1', 'b2', 'c3'], board)
+puts player1.positions_on_board
 puts board.display_grid
