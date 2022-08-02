1a66e9d95be15122ee499b99945978eb663db693
JP <85654561+jp524@users.noreply.github.com>
2022-08-02 17:07:20 -0400

    Build run_game method with one player

--- game.rb
+++ game.rb
@@ -2,7 +2,7 @@ class Board
   attr_reader :positions
 
   def initialize
-    @positions = ['a1', 'b1', 'c1', 'a2', 'b2', 'c2', 'a3', 'b3', 'c3']
+    @positions = %w[a1 b1 c1 a2 b2 c2 a3 b2 c3]
   end
 
   def display_grid
@@ -13,11 +13,12 @@ class Board
 end
 
 class Player
-  attr_reader :board
+  attr_reader :board, :name, :choices
 
-  def initialize(marker, choices, board)
+  def initialize(name, marker, board)
+    @name = name
     @marker = marker
-    @choices = choices
+    @choices = []
     @board = board
   end
 
@@ -32,15 +33,42 @@ class Player
       @choices.push(position)
       positions_on_board
     else
-      p 'This spot is already taken. Choose another one.'
+      p 'This cell is already taken or does not exist. Please choose another one.'
     end
   end
 end
 
-board = Board.new
-player1 = Player.new('X', ['a1', 'b2', 'c3'], board)
-player2 = Player.new('O', ['a2', 'b3', 'c1'], board)
-player1.positions_on_board
-player2.positions_on_board
-player1.new_move('b1')
-puts board.display_grid
+def run_game
+  board = Board.new
+  player1 = Player.new('Player 1', 'X', board)
+  player2 = Player.new('Player 2', 'O', board)
+
+  victory_combinations = [%w[a1 b1 c1],
+                          %w[a2 b2 c2],
+                          %w[a3 b3 c3],
+                          %w[a1 a2 a3],
+                          %w[b1 b2 b3],
+                          %w[c1 c2 c3],
+                          %w[a1 b2 c3],
+                          %w[a3 b2 c1]]
+  game_over = false
+  player_turn = player1
+
+  puts "\nWelcome to Tic-Tac-Toe!\n\n"
+  puts board.display_grid
+  puts "\n"
+
+  while game_over == false
+    puts "#{player_turn.name}: Enter the name of a cell on the board. E.g. 'a1' or 'c2'."
+    player_turn.new_move(gets.chomp)
+    puts board.display_grid
+    puts "\n"
+
+    if victory_combinations.include?(player_turn.choices)
+      game_over = true
+      puts "#{player_turn.name} wins!"
+    end
+  end
+end
+
+run_game
